2890325b0370f23eb7e32b9d35ca2efb9dcc0ca1
Luis <jrmc123lhh@gmail.com>
2021-02-27 11:41:35 -0600

    lhh
    ok

---
+++ ControlProductos/Entity/JDETwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlProductos.Entity
+{
+    public class GetJDETwosResult_
+    {
+        public List<JDETwo> GetJDETwosResult { get; set; }
+    }
+
+    public class JDETwo
+    {
+        public int JDEID { get; set; }
+        public string codigo { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ ControlProductos/dataAccess/JDETwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ControlProductos.dataAccess
+{
+    public class JDETwoda : Base
+    {
+        public List<Entity.JDETwo> GetCombo()
+        {
+            string json = methodGet("GetJDETwos");
+            Entity.GetJDETwosResult_ regreso = JsonConvert.DeserializeObject<Entity.GetJDETwosResult_>(json);
+            return regreso.GetJDETwosResult;
+        }
+    }
+}
